108528cfb1c4da1dee0f536169b886ab945c2b85
Daria <daszka_s@interia.pl>
2022-10-03 13:34:01 +0200

    Remove comments

--- index.js
+++ index.js
@@ -11,27 +11,15 @@ function isLongEnough(word){
 };
 const allLongEnough = wordsArray.filter(isLongEnough);
 
-// has to have 1 center char
 function hasCenterLetter(word){
     return word.toLowerCase().includes(centerLetter);
 };
 const withCenterLetter = allLongEnough.filter(hasCenterLetter);
 
-// cannot have different chars than passed
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const otherLetters = arguments[1].split("");
 const diff = alphabet.filter(letter => !otherLetters.includes(letter));
 const forbiddenLetters = diff.filter(letter => letter !== centerLetter);
-
-// function noForbiddenLetters(word){
-//     function isInWord(forbiddenLetter){
-//         return word.toLowerCase().includes(forbiddenLetter)
-//     }
-//     const forbiddenLettersInWord = forbiddenLetters.filter(isInWord);
-//     const hasForbidden = forbiddenLettersInWord.length > 0;
-//     return !hasForbidden;
-// }
-// console.time('first')
 
 let allowedWords = withCenterLetter;
 for (const letter of forbiddenLetters) {
@@ -40,9 +28,5 @@ for (const letter of forbiddenLetters) {
     }
     allowedWords = allowedWords.filter(hasNoForbidden);
 }
-// console.timeEnd("first")
 
-// console.time('second')
-// const finalWords = withCenterLetter.filter(noForbiddenLetters);
-// console.timeEnd("second")
 console.log(allowedWords);
